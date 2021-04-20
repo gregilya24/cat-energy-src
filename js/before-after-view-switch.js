25d57894f7aacb94e895dc60094948f3e824954e
gregilya24 <56547694+gregilya24@users.noreply.github.com>
2021-04-20 11:33:39 +0700

    Add files via upload

--- js/before-after-view-switch.js
+++ js/before-after-view-switch.js
@@ -1,6 +1,5 @@
 var imgBefore = document.querySelector('.viewer__view-before');
 var imgAfter = document.querySelector('.viewer__view-after');
-// var scale = progress.querySelector('.demo__progress-bar');
 var range = document.querySelector('.viewer__range');
 
 
@@ -9,20 +8,59 @@ var link = document.querySelector(".viewer__control-switch");
 var moddd = document.querySelector(".viewer__view-before");
 var moddd2 = document.querySelector(".viewer__view-after");
 
-
-
-
-window.onresize = function (event) {
-  if (window.innerWidth >= 768) {
-    moddd2.classList.remove("viewer__view--mob-hidden");
-    moddd.classList.remove("viewer__view--mob-hidden");
-    console.log("убрать скрытие после");
-  }
-  else {
-    moddd2.classList.add("viewer__view--mob-hidden");
-    console.log("добавить скрытие после");
-  }
+if (window.innerWidth >= 768) {
+  moddd2.classList.remove("viewer__view--mob-hidden");
+  imgBefore.style.width = 50 + '%';
+  imgAfter.style.width = 50 + '%';
+  imgBefore.style.backgroundposition = 'center left';
+  imgAfter.style.backgroundposition = 'center right';
+  console.log("ширина 50%, оба видны");
+} else {
+  moddd.classList.remove("viewer__view--mob-hidden");
+  moddd2.classList.add("viewer__view--mob-hidden");
+  imgBefore.style.width = 100 + '%';
+  imgAfter.style.width = 100 + '%';
+  console.log("ширина 100%, виден первый, второй нет");
 }
+
+
+
+
+
+// if (window.innerWidth >= 768) {
+//   moddd2.classList.remove("viewer__view--mob-hidden");
+//   moddd.classList.remove("viewer__view--mob-hidden");
+//   console.log("убрать скрытие после");
+// }
+// else {
+//   moddd2.classList.add("viewer__view--mob-hidden");
+//   console.log("добавить скрытие после");
+// }
+window.addEventListener('resize', function(){
+  console.log("смена шиирны окна");
+    if (window.innerWidth >= 768) {
+      moddd2.classList.remove("viewer__view--mob-hidden");
+      moddd.classList.remove("viewer__view--mob-hidden");
+      console.log("убрать скрытие после");
+    }
+    else {
+      moddd2.classList.add("viewer__view--mob-hidden");
+      console.log("добавить скрытие после");
+    }
+}, true);
+
+// window.onresize = function () {
+//   console.log("смена шиирны окна");
+//   if (window.innerWidth >= 768) {
+//     moddd2.classList.remove("viewer__view--mob-hidden");
+//     moddd.classList.remove("viewer__view--mob-hidden");
+//     console.log("убрать скрытие после");
+//   }
+//   else {
+//     moddd2.classList.add("viewer__view--mob-hidden");
+//     console.log("добавить скрытие после");
+//   }
+// }
 
 
 link.addEventListener("click", function (evt) {
@@ -44,18 +82,3 @@ range.addEventListener('input', function() {
   imgBefore.style.width = (100 - range.value) + '%';
   imgAfter.style.width = range.value + '%';
 })
-
-
-if (window.innerWidth >= 768) {
-  moddd2.classList.remove("viewer__view--mob-hidden");
-  imgBefore.style.width = 50 + '%';
-  imgAfter.style.width = 50 + '%';
-  imgBefore.style.backgroundposition = 'center left';
-  imgAfter.style.backgroundposition = 'center right';
-} else {
-  moddd2.classList.add("viewer__view--mob-hidden");
-  imgBefore.style.width = 100 + '%';
-  imgAfter.style.width = 100 + '%';
-  // imgBefore.style.backgroundposition = 'center top';
-  // imgAfter.style.backgroundposition = 'center top';
-}
